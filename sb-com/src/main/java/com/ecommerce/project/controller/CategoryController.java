9e9754ba8572630f42645ec2a8f2f96d8573a80f
saklain-s <123095572+saklain-s@users.noreply.github.com>
2025-05-24 00:12:15 +0530

    Added hibernate

--- sb-com/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-com/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
@@ -19,20 +20,24 @@ public class CategoryController {
 
 
 
-    @GetMapping("/api/public/categories")
+    @GetMapping("/public/categories")
+   // @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity <List<Category>> getAllCategories(){
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
 
-    @PostMapping("/api/public/categories")
+    @PostMapping("/public/categories")
+   // @RequestMapping(value = "/public/categories", method = RequestMethod.POST)
+
     public ResponseEntity<String>  createCategory(@RequestBody Category category){
         categoryService.createCatergory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
-  //6:22:24
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+
+    @DeleteMapping("admin/categories/{categoryId}")
+
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
         try {
             String status = categoryService.deleteCategory(categoryId);
@@ -46,7 +51,8 @@ public class CategoryController {
 
     }
 
-    @PutMapping("/api/public/categories/{categoryId}")
+    @PutMapping("public/categories/{categoryId}")
+
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
         try{
             Category savedCategory = categoryService.updateCategory(category, categoryId);
